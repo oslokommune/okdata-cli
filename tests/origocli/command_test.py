7708e18516691f1e048ee9496857ca641c6457e8
torklo <tord.kloster@knowit.no>
2020-01-09 12:45:16 +0100

    DP-323 update sdk version, update test

--- tests/origocli/command_test.py
+++ tests/origocli/command_test.py
@@ -1,20 +1,17 @@
 import sys
 
-from bin.cli import main
+from origocli.commands.datasets import DatasetsCommand
 
 
 def test_command_get_cmd():
-    args = {}
-    args["ls"] = True
-    args["<datasetid>"] = True
-    args["--format"] = True
+    args = ["datasets", "ls", "datasetid", "--format", "something"]
     old_sys_argv = sys.argv
     sys.argv = [old_sys_argv[0]] + args
-    cmd = main()
+    cmd = DatasetsCommand()
 
     assert cmd.cmd("ls") is True
-    assert cmd.arg("datasetid") is True
-    assert cmd.opt("format") is True
+    assert cmd.arg("datasetid") == "datasetid"
+    assert cmd.opt("format") == "something"
     assert cmd.cmd("notexisting") is None
     assert cmd.arg("notexisting") is None
     assert cmd.opt("notexisting") is None
