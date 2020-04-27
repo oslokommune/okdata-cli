584a9012277f5f871e0c73eedf17bffd0f3df83a
Håvard Eide <havard.eide@byr.oslo.kommune.no>
2020-04-27 15:52:52 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="origo-cli",
-    version="0.1.0",
+    version="0.2.0",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
     description="CLI for origo",
