ae6ae756bf67622ae06c4ac23efa19b0133b62bd
Håvard Eide <havard.eide@byr.oslo.kommune.no>
2020-05-05 20:58:06 +0200

    DP-793 - fix test after changing auto-aligning rows

--- tests/origocli/command_test.py
+++ tests/origocli/command_test.py
@@ -102,7 +102,7 @@ def test_pretty_print_success(capsys):
     assert (
         captured.out
         == """+-------+-------+
-|  name |  key  |
+| name  | key   |
 +-------+-------+
 | hello | world |
 +-------+-------+
