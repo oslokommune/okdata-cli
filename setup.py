828f247773feda8cb9ccc0771b5a18615041f89c
Simen Heggestøyl <simen.heggestoyl@byr.oslo.kommune.no>
2020-08-26 10:10:08 +0200

    DP-1034 Ready for PyPI
    Ready the package for distribution at PyPI.

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setuptools.setup(
     version="0.2.0",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
-    description="CLI for origo",
+    description="CLI for services provided by Oslo Origo",
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    url="https://github.com/oslokommune/origo-cli/",
+    packages=setuptools.find_packages(".", exclude=["tests*"]),
     install_requires=[
         "PrettyTable",
         "docopt",
@@ -23,4 +25,10 @@ setuptools.setup(
         "pygments",
     ],
     entry_points={"console_scripts": ["origo=bin.cli:main"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
