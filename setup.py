7bccc4d58f621792520c1011dfdd41971ed25fb0
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-10-07 09:32:16 +0200

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="origo-cli",
-    version="0.4.0",
+    version="0.5.0",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
     description="CLI for services provided by Oslo Origo",
