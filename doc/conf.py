310fef8f6c5697256d07d60e96db495ccf904222
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-10-07 10:28:35 +0200

    Bump doc conf 0.5.0

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Oslo Origo"
 author = "Oslo Origo"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.5.0"
 
 
 # -- General configuration ---------------------------------------------------
