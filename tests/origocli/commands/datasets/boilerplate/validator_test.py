650ec46b72ef80b34b5bb37bcf79daf9d790b969
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2021-01-06 12:38:11 +0100

    DP-1556: Add tests for new validators

--- tests/origocli/commands/datasets/boilerplate/validator_test.py
+++ tests/origocli/commands/datasets/boilerplate/validator_test.py
@@ -8,6 +8,9 @@ from okdata.cli.commands.datasets.boilerplate.validator import (
     PhoneValidator,
     SimpleEmailValidator,
     TitleValidator,
+    StandardsValidator,
+    SpatialValidator,
+    SpatialResolutionValidator,
 )
 
 # Note: no testing of return values since the validator is only
@@ -107,3 +110,81 @@ class TestKeywordValidator:
     def test_empty_keywords(self):
         with pytest.raises(ValidationError):
             self.validate_keywords({"text": ""})
+
+
+class TestStandardsValidator:
+    def validate_document(self, data):
+        validator = StandardsValidator()
+        document = SimpleNamespace(**data)
+        validator.validate(document)
+
+    def test_valid(self):
+        self.validate_document({"text": "abc"})
+        self.validate_document({"text": "http://www.opengis.net/def/crs/EPSG/0/5972"})
+        self.validate_document({"text": "abc\ndefg"})
+
+    def test_empty(self):
+        self.validate_document({"text": ""})
+        self.validate_document({"text": "\n\n\n"})
+
+    def test_min_length(self):
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "a"})
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "a\n\nc\nd"})
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "\n\nc\nd"})
+
+    def test_one_invalid_and_one_valid_value(self):
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "a\nstandarddokument"})
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "standarddokument\nab"})
+
+
+class TestSpatialValidator:
+    def validate_document(self, data):
+        validator = SpatialValidator()
+        document = SimpleNamespace(**data)
+        validator.validate(document)
+
+    def test_valid(self):
+        self.validate_document({"text": "Oslo"})
+        self.validate_document({"text": "Oslo Bydel 1\nOslo Bydel 2"})
+
+    def test_empty(self):
+        self.validate_document({"text": ""})
+        self.validate_document({"text": "\n\n\n"})
+
+    def test_one_invalid_and_one_valid_value(self):
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "Å\n"})
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "\nÅs"})
+
+
+class TestSpatialResolutionValidator:
+    def validate_document(self, data):
+        validator = SpatialResolutionValidator()
+        document = SimpleNamespace(**data)
+        validator.validate(document)
+
+    def test_valid_numbers(self):
+        self.validate_document({"text": "1"})
+        self.validate_document({"text": "1.234"})
+        self.validate_document({"text": "1,234"})
+        self.validate_document({"text": "0,234"})
+
+    def test_invalid_separator(self):
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "1-234"})
+
+    def test_gt_zero(self):
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "0"})
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "-1.2"})
+
+    def test_invalid_value(self):
+        with pytest.raises(ValidationError):
+            self.validate_document({"text": "ukjent"})
