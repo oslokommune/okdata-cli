068ab9ffcb20b64803dbc4448171cc3a6f4f7eb5
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-02-15 12:22:18 +0100

    DP-1350 Offer event pipeline choices too

--- okdata/cli/commands/datasets/boilerplate/config.py
+++ okdata/cli/commands/datasets/boilerplate/config.py
@@ -14,14 +14,25 @@ from .validator import (
 
 required_style = Style([("qmark", "fg:red bold")])
 
-pipeline_choices = [
-    Choice("Lagre dataen slik den er", "data-copy"),
-    Choice("Konverter fra CSV til Parquet", "csv-to-parquet"),
-    Choice("Konverter fra Excel til CSV", "pipeline-excel-to-csv"),
-    Choice("Ingen prosessering (krever manuell konfigurasjon av pipeline)", False),
-]
+pipeline_choices = {
+    "file": [
+        Choice("Lagre dataen slik den er", "data-copy"),
+        Choice("Konverter fra CSV til Parquet", "csv-to-parquet"),
+        Choice("Konverter fra Excel til CSV", "pipeline-excel-to-csv"),
+        Choice("Ingen prosessering (krever manuell konfigurasjon av pipeline)", False),
+    ],
+    "event": [
+        Choice("Bruk dataen slik den er", "event-copy"),
+        Choice("Ingen prosessering (krever manuell konfigurasjon av pipeline)", False),
+    ],
+}
 
-available_pipelines = [c.value for c in pipeline_choices if c.value]
+available_pipelines = [
+    choice.value
+    for category in pipeline_choices.values()
+    for choice in category
+    if choice.value
+]
 
 
 def filter_comma_separated(value):
@@ -139,8 +150,8 @@ def boilerplate_prompt(include_extra_metadata=True):
             "style": required_style,
             "name": "pipeline",
             "message": "Prosessering",
-            "choices": pipeline_choices,
-            "when": lambda x: x["sourceType"] == "file",
+            "choices": lambda x: pipeline_choices[x["sourceType"]],
+            "when": lambda x: x["sourceType"] in ["file", "event"],
         },
     ]
 
