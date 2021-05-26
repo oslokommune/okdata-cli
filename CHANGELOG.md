e022594885ee476030228128a1466864045bcb5e
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-05-26 15:53:19 +0200

    Bump version: 0.11.0 → 0.12.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## ?.?.?
+## 0.12.0
 
 * The new permission API based `WebhookClient` from the SDK is now used instead
   of the deprecated `SimpleDatasetAuthorizerClient`.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="okdata-cli",
-    version="0.11.0",
+    version="0.12.0",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
     description="CLI for services provided by Oslo Origo",
