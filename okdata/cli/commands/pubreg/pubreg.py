431af6ef7c436488dc10e27e3c83e252b293bf17
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-09-21 10:29:58 +0200

    DP-2288 Add rogue command `pubreg create-client`
    Add another rogue public register command, this time for creating new
    Maskinporten clients.

--- okdata/cli/commands/pubreg/pubreg.py
+++ okdata/cli/commands/pubreg/pubreg.py
@@ -1,7 +1,11 @@
+import uuid
 from operator import itemgetter
+
+from requests.exceptions import HTTPError
 
 from okdata.cli.command import BASE_COMMAND_OPTIONS, BaseCommand
 from okdata.cli.commands.pubreg.client import PubregClient
+from okdata.cli.commands.pubreg.wizards import ClientCreateWizard
 from okdata.cli.output import create_output
 
 
@@ -9,9 +13,11 @@ class PubregCommand(BaseCommand):
     __doc__ = f"""Oslo :: Public registers
 
 Usage:
+  okdata pubreg create-client [--env=<env>]
   okdata pubreg list-keys <maskinporten-env> <client-id> [--env=<env>]
 
 Examples:
+  okdata pubreg create-client
   okdata pubreg list-keys test my-client
 
 Options:{BASE_COMMAND_OPTIONS}
@@ -22,8 +28,32 @@ Options:{BASE_COMMAND_OPTIONS}
         self.client = PubregClient(env=self.opt("env"))
 
     def handler(self):
+        if self.cmd("create-client"):
+            self.create_client()
         if self.cmd("list-keys"):
             self.list_client_keys(self.arg("maskinporten-env"), self.arg("client-id"))
+
+    def create_client(self):
+        config = ClientCreateWizard().start()
+
+        team = config["team"]
+        provider = config["provider"]
+        env = config["environment"]
+        scopes = config["scopes"]
+
+        name = f"{team}-{provider}-{env}-{uuid.uuid4()}"
+
+        self.print(f"Will create a new client '{name}' in {env} with scopes {scopes}.")
+        if input("Continue? [y/N]: ") != "y":
+            self.print("Aborted")
+            return
+
+        try:
+            self.print("Creating '{name}'...")
+            self.client.create_client(env, name, scopes)
+            self.print("Done!")
+        except HTTPError as e:
+            self.print(f"Something went wrong: {e}")
 
     def list_client_keys(self, env, client_id):
         keys = self.client.get_keys(env, client_id)
