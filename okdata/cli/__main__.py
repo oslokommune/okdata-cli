9b47f0c961a146c4106772dc44f7cf114d71ecb2
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2022-04-25 11:57:15 +0200

    T#167 Handle more general request errors
    Handle more general exceptions from the `requests` library, such as
    connection errors and retry errors.

--- okdata/cli/__main__.py
+++ okdata/cli/__main__.py
@@ -1,10 +1,9 @@
 import json
 import sys
 
-from requests.exceptions import HTTPError
 from keycloak.exceptions import KeycloakGetError
-
 from okdata.sdk.exceptions import ApiAuthenticateError
+from requests.exceptions import RequestException
 
 from okdata.cli.command import BaseCommand
 from okdata.cli.commands.datasets import DatasetsCommand
@@ -29,25 +28,34 @@ def main():
         try:
             instance.login()
             instance.handle()
-        except HTTPError as he:
-            instance.print_error_response(he.response.json())
+        except RequestException as e:
+            if hasattr(e.response, "json"):
+                instance.print_error_response(e.response.json())
+            else:
+                instance.print(
+                    "A server error occurred. Please try again, or contact "
+                    "Datapatruljen if the problem persists.",
+                )
         except ApiAuthenticateError:
             instance.print(
-                "An error occured (ApiAuthenticateError): Invalid credentials",
+                "An error occurred (ApiAuthenticateError): Invalid credentials",
                 {"error": 1, "message": "Invalid credentials"},
             )
         except KeycloakGetError as e:
             error = json.loads(e.error_message)
             instance.log.info(f"Keycloak reported: {e}")
             instance.print(
-                f"An error occured (KeycloakGetError): {error['error_description']}"
+                f"An error occurred (KeycloakGetError): {error['error_description']}"
             )
         except Exception as e:
             instance.print(
-                "A Exception occured",
+                "An exception occurred",
                 {
                     "error": 1,
-                    "message": "okdata-cli failed with exception, see log output for more information",
+                    "message": (
+                        "okdata-cli failed with an exception, see log output "
+                        "for more information",
+                    ),
                 },
             )
             instance.log.exception(f"okdata-cli failed with: {e}")
