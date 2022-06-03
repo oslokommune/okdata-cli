a05f4f0319817c44146a27f2b1993a0be5b82544
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2022-06-03 14:22:15 +0200

    T#223 Clarify Maskinporten environment input
    Clarify that we're after the Maskinporten environment in every
    `pubreg` sub-command. Every sub-command is turned into a wizard,
    avoiding having to specify the environment on the command line (as a
    nice side-effect, users no longer have to copy/paste client IDs and
    key IDs between commands).
    
    We might look into adding non-interactive support for all these
    commands later, if there is demand for it.

---
+++ okdata/cli/commands/pubreg/questions.py
@@ -0,0 +1,200 @@
+import re
+from operator import itemgetter
+
+from prompt_toolkit.styles import Style
+from questionary import Choice
+
+_providers = {
+    "freg": "Folkeregisteret",
+    "krr": "Kontaktregisteret",
+    "skatt": "Skatteetaten",
+}
+
+_scopes = {
+    "freg": [
+        "folkeregister:deling/offentligmedhjemmel",
+        "folkeregister:deling/offentligutenhjemmel",
+    ],
+    "krr": [
+        "krr:global/digitalpost.read",
+        "krr:global/kontaktinformasjon.read",
+    ],
+    "skatt": [
+        "skatteetaten:arbeidsgiveravgift",
+        "skatteetaten:avregning",
+        "skatteetaten:boligsparingforungdom",
+        "skatteetaten:inntekt",
+        "skatteetaten:inntektsmottakere",
+        "skatteetaten:mvameldingsopplysning",
+        "skatteetaten:oppdragutenlandskevirksomheter",
+        "skatteetaten:restanser",
+        "skatteetaten:skattemelding",
+        "skatteetaten:skatteplikt",
+        "skatteetaten:spesifisertsummertskattegrunnlag",
+        "skatteetaten:summertskattegrunnlag",
+        "skatteetaten:tjenestepensjonsavtale",
+    ],
+}
+
+_environments = [
+    "test",
+    "prod",
+]
+
+_aws_regions = [
+    ("eu-central-1 (Frankfurt)", "eu-central-1"),
+    ("eu-north-1 (Stockholm)", "eu-north-1"),
+    ("eu-west-1 (Ireland)", "eu-west-1"),
+]
+
+
+class NoClientsError(Exception):
+    pass
+
+
+class NoTeamError(Exception):
+    pass
+
+
+_common_style = {
+    "qmark": "*",
+    "style": Style([("qmark", "fg:red bold")]),
+}
+
+
+def q_env():
+    return {
+        **_common_style,
+        "type": "select",
+        "name": "env",
+        "message": "Maskinporten environment",
+        "choices": _environments,
+    }
+
+
+def q_team(team_client):
+    def _team_choices():
+        teams = team_client.get_teams(has_role="origo-team")
+
+        if not teams:
+            raise NoTeamError
+
+        return [Choice(t["name"], t["id"]) for t in teams]
+
+    return {
+        **_common_style,
+        "type": "select",
+        "name": "team_id",
+        "message": "Team",
+        "choices": _team_choices(),
+    }
+
+
+def q_provider():
+    return {
+        **_common_style,
+        "type": "select",
+        "name": "provider_id",
+        "message": "Provider",
+        "choices": [Choice(pname, pid) for pid, pname in _providers.items()],
+    }
+
+
+def q_scopes():
+    return {
+        **_common_style,
+        "type": "checkbox",
+        "name": "scopes",
+        "message": "Scopes",
+        "choices": lambda x: _scopes[x["provider_id"]],
+        "validate": (lambda choices: bool(choices) or "Select at least one scope"),
+    }
+
+
+def q_integration():
+    def _validate_integration(text):
+        if len(text) > 30:
+            return "Too long!"
+        if not re.fullmatch("[0-9a-z-]+", text):
+            return 'Only lowercase letters, numbers and "-", please'
+        return True
+
+    return {
+        **_common_style,
+        "type": "text",
+        "name": "integration",
+        "message": "Component/integration name",
+        "validate": _validate_integration,
+    }
+
+
+def q_client(pubreg_client):
+    def _client_choices(env):
+        clients = pubreg_client.get_clients(env)
+
+        if not clients:
+            raise NoClientsError
+
+        return [
+            Choice(
+                c["client_name"],
+                {"id": c["client_id"], "name": c["client_name"]},
+            )
+            for c in sorted(clients, key=itemgetter("client_name"))
+        ]
+
+    return {
+        **_common_style,
+        "type": "select",
+        "name": "client",
+        "message": "Client",
+        "choices": lambda x: _client_choices(x["env"]),
+    }
+
+
+def q_send_to_aws():
+    return {
+        **_common_style,
+        "type": "confirm",
+        "name": "send_to_aws",
+        "message": "Send key to AWS Parameter Store?",
+        "auto_enter": False,
+    }
+
+
+def q_aws_account():
+    return {
+        **_common_style,
+        "type": "text",
+        "name": "aws_account",
+        "message": "AWS account number",
+        "when": lambda x: x["send_to_aws"],
+        "validate": (
+            lambda t: bool(re.fullmatch("[0-9]{12}", t)) or "12 digits, please"
+        ),
+    }
+
+
+def q_aws_region():
+    return {
+        **_common_style,
+        "type": "select",
+        "name": "aws_region",
+        "message": "AWS region",
+        "when": lambda x: x["send_to_aws"],
+        "choices": [Choice(*r) for r in _aws_regions],
+    }
+
+
+def q_key(pubreg_client):
+    def _key_choices(env, client_id):
+        keys = pubreg_client.get_keys(env, client_id)
+        return sorted([k["kid"] for k in keys])
+
+    return {
+        **_common_style,
+        "type": "select",
+        "name": "key_id",
+        "message": "Key",
+        "choices": lambda x: _key_choices(x["env"], x["client"]["id"]),
+    }
--- okdata/cli/commands/pubreg/wizards.py
+++ okdata/cli/commands/pubreg/wizards.py
@@ -1,212 +1,103 @@
-import re
-from operator import itemgetter
+from questionary import prompt
 
-from prompt_toolkit.styles import Style
-from questionary import Choice, prompt
-
-required_style = Style([("qmark", "fg:red bold")])
-
-
-_providers = {
-    "freg": "Folkeregisteret",
-    "krr": "Kontaktregisteret",
-    "skatt": "Skatteetaten",
-}
-
-_scopes = {
-    "freg": [
-        "folkeregister:deling/offentligmedhjemmel",
-        "folkeregister:deling/offentligutenhjemmel",
-    ],
-    "krr": [
-        "krr:global/digitalpost.read",
-        "krr:global/kontaktinformasjon.read",
-    ],
-    "skatt": [
-        "skatteetaten:arbeidsgiveravgift",
-        "skatteetaten:avregning",
-        "skatteetaten:boligsparingforungdom",
-        "skatteetaten:inntekt",
-        "skatteetaten:inntektsmottakere",
-        "skatteetaten:mvameldingsopplysning",
-        "skatteetaten:oppdragutenlandskevirksomheter",
-        "skatteetaten:restanser",
-        "skatteetaten:skattemelding",
-        "skatteetaten:skatteplikt",
-        "skatteetaten:spesifisertsummertskattegrunnlag",
-        "skatteetaten:summertskattegrunnlag",
-        "skatteetaten:tjenestepensjonsavtale",
-    ],
-}
-
-_environments = [
-    "test",
-    "prod",
-]
-
-_aws_regions = [
-    ("eu-central-1 (Frankfurt)", "eu-central-1"),
-    ("eu-north-1 (Stockholm)", "eu-north-1"),
-    ("eu-west-1 (Ireland)", "eu-west-1"),
-]
+from okdata.cli.commands.pubreg.questions import (
+    _providers,
+    q_aws_account,
+    q_aws_region,
+    q_client,
+    q_env,
+    q_integration,
+    q_key,
+    q_provider,
+    q_scopes,
+    q_send_to_aws,
+    q_team,
+)
 
 
-class NoClientsError(Exception):
-    pass
-
-
-class CreateClientWizard:
-    """Wizard for the `pubreg create-client` command."""
-
-    def __init__(self, teams):
-        self.teams = teams
-
-    def _team_choices(self):
-        return [Choice(t["name"], t["id"]) for t in self.teams]
-
-    def _validate_integration(self, text):
-        if len(text) > 30:
-            return "Too long!"
-        if not re.fullmatch("[0-9a-z-]+", text):
-            return 'Only lowercase letters, numbers and "-", please'
-        return True
-
-    def start(self):
-        choices = prompt(
-            [
-                {
-                    "type": "select",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "env",
-                    "message": "Environment",
-                    "choices": _environments,
-                },
-                {
-                    "type": "select",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "team_id",
-                    "message": "Team",
-                    "choices": lambda x: self._team_choices(),
-                },
-                {
-                    "type": "select",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "provider_id",
-                    "message": "Provider",
-                    "choices": [
-                        Choice(pname, pid) for pid, pname in _providers.items()
-                    ],
-                },
-                {
-                    "type": "checkbox",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "scopes",
-                    "message": "Scopes",
-                    "choices": lambda x: _scopes[x["provider_id"]],
-                    "validate": (
-                        lambda choices: bool(choices) or "Select at least one scope"
-                    ),
-                },
-                {
-                    "type": "text",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "integration",
-                    "message": "Component/integration name",
-                    "validate": self._validate_integration,
-                },
-            ]
-        )
-
-        return {
-            "env": choices["env"],
-            "team_id": choices.get("team_id"),
-            "provider_id": choices["provider_id"],
-            "provider_name": _providers[choices["provider_id"]],
-            "integration": choices["integration"],
-            "scopes": choices["scopes"],
-        }
-
-
-class CreateKeyWizard:
-    """Wizard for the `pubreg create-key` command."""
-
-    def __init__(self, client):
-        self.client = client
-
-    def _client_choices(self, env):
-        clients = self.client.get_clients(env)
-
-        if not clients:
-            raise NoClientsError
-
-        return [
-            Choice(
-                c["client_name"],
-                {"id": c["client_id"], "name": c["client_name"]},
-            )
-            for c in sorted(clients, key=itemgetter("client_name"))
+def create_client_wizard(team_client):
+    choices = prompt(
+        [
+            q_env(),
+            q_team(team_client),
+            q_provider(),
+            q_scopes(),
+            q_integration(),
         ]
+    )
+    return {
+        "env": choices["env"],
+        "team_id": choices.get("team_id"),
+        "provider_id": choices["provider_id"],
+        "provider_name": _providers[choices["provider_id"]],
+        "integration": choices["integration"],
+        "scopes": choices["scopes"],
+    }
 
-    def start(self):
-        choices = prompt(
-            [
-                {
-                    "type": "select",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "env",
-                    "message": "Environment",
-                    "choices": _environments,
-                },
-                {
-                    "type": "select",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "client",
-                    "message": "Client",
-                    "choices": lambda x: self._client_choices(x["env"]),
-                },
-                {
-                    "type": "confirm",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "send_to_aws",
-                    "message": "Send key to AWS Parameter Store?",
-                    "auto_enter": False,
-                },
-                {
-                    "type": "text",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "aws_account",
-                    "message": "AWS account number",
-                    "when": lambda x: x["send_to_aws"],
-                    "validate": (
-                        lambda t: bool(re.fullmatch("[0-9]{12}", t))
-                        or "12 digits, please"
-                    ),
-                },
-                {
-                    "type": "select",
-                    "qmark": "*",
-                    "style": required_style,
-                    "name": "aws_region",
-                    "message": "AWS region",
-                    "when": lambda x: x["send_to_aws"],
-                    "choices": [Choice(*r) for r in _aws_regions],
-                },
-            ]
-        )
 
-        return {
-            "env": choices["env"],
-            "client_id": choices["client"]["id"],
-            "client_name": choices["client"]["name"],
-            "aws_account": choices.get("aws_account"),
-            "aws_region": choices.get("aws_region"),
-        }
+def list_clients_wizard():
+    choices = prompt([q_env()])
+    return {"env": choices["env"]}
+
+
+def delete_client_wizard(pubreg_client):
+    choices = prompt(
+        [
+            q_env(),
+            q_client(pubreg_client),
+        ]
+    )
+    return {
+        "env": choices["env"],
+        "client_id": choices["client"]["id"],
+        "client_name": choices["client"]["name"],
+    }
+
+
+def create_key_wizard(pubreg_client):
+    choices = prompt(
+        [
+            q_env(),
+            q_client(pubreg_client),
+            q_send_to_aws(),
+            q_aws_account(),
+            q_aws_region(),
+        ]
+    )
+    return {
+        "env": choices["env"],
+        "client_id": choices["client"]["id"],
+        "client_name": choices["client"]["name"],
+        "aws_account": choices.get("aws_account"),
+        "aws_region": choices.get("aws_region"),
+    }
+
+
+def list_keys_wizard(pubreg_client):
+    choices = prompt(
+        [
+            q_env(),
+            q_client(pubreg_client),
+        ]
+    )
+    return {
+        "env": choices["env"],
+        "client_id": choices["client"]["id"],
+        "client_name": choices["client"]["name"],
+    }
+
+
+def delete_key_wizard(pubreg_client):
+    choices = prompt(
+        [
+            q_env(),
+            q_client(pubreg_client),
+            q_key(pubreg_client),
+        ]
+    )
+    return {
+        "env": choices["env"],
+        "client_id": choices["client"]["id"],
+        "client_name": choices["client"]["name"],
+        "key_id": choices["key_id"],
+    }
